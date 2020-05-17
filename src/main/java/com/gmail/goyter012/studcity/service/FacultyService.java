b6d9e8d822677b98c69a6adf0f2bab1c60fda69f
semerey <fyzctvthtq@gmail.com>
2020-05-17 12:14:26 +0300

    added faculty's repo and service

---
+++ src/main/java/com/gmail/goyter012/studcity/service/FacultyService.java
@@ -0,0 +1,47 @@
+package com.gmail.goyter012.studcity.service;
+
+import com.gmail.goyter012.studcity.model.Faculty;
+import com.gmail.goyter012.studcity.repo.FacultyRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FacultyService {
+
+
+    private FacultyRepo facultyRepo;
+
+    @Autowired
+    public void setFacultyRepo(FacultyRepo facultyRepo) {
+        this.facultyRepo = facultyRepo;
+    }
+
+    public List<Faculty> allFaculties(){
+        return (List<Faculty>) facultyRepo.findAll();
+    }
+
+
+    public Faculty getFacultyById(Long id){
+        return facultyRepo.findById(id).get();
+
+    }
+
+    public void delete(Faculty faculty){
+        facultyRepo.delete(faculty);
+
+    }
+
+
+    public Faculty saveFaculty(Faculty faculty){
+        facultyRepo.save(faculty);
+        return faculty;
+    }
+
+    public List<Faculty> getAllFacultiesByName(String name){
+        return facultyRepo.findAllByName(name);
+    }
+}
